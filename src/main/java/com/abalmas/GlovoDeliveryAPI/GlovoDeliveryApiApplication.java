f94e7330e5a556e7879fd6ebeafdd3f6ba281ce8
Dmytro Abalmas <abalmasjava@gmail.com>
2023-12-29 15:06:48 +0200

    Initial commit

---
+++ src/main/java/com/abalmas/GlovoDeliveryAPI/GlovoDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.abalmas.GlovoDeliveryAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlovoDeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlovoDeliveryApiApplication.class, args);
+	}
+
+}
